ac543346275c549ea9c76207b8ed6e7798b2c0fa
= <mrdakadoc@gmail.com>
2011-07-14 17:02:43 -0700

    test'

---
+++ lib/active_cache_record.rb
@@ -0,0 +1,29 @@
+class ActiveCacheRecord < ActiveRecord::Base
+
+  def find(*args)
+
+  end
+
+
+
+  private
+  def cache_delete
+
+  end
+
+  def increment_counter
+
+  end
+
+  def drecement_counter
+
+  end
+
+  def destroy
+
+  end
+  
+
+
+
+end
