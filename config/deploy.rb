a1854a16d9d0aa271bfef49c71ef00702b2480bc
= <mrdakadoc@gmail.com>
2011-08-03 13:10:46 -0700

    packing gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,23 @@
 set :application, "sc2sl"
-set :domain,      "http://vps11537.ovh.net/"
+set :domain,      "vps11537.ovh.net"
 set :repository,  "git@github.com:turlockmike/sc2sl.git"
+set :deploy_to,   "/apps/#{application}"
 set :branch, "master"
 set :scm, "git"
 set :scm_verbose, true
 set :user, "root"
-set :scm_passphrase, "t9d7ae8v"
+
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-rrole :app, domain
+role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+
+
+#require 'bundler/capistrano'
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
@@ -23,3 +27,5 @@ namespace :deploy do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
  end
+
+#after 'deploy:update_code', 'deploy:symlink_shared'
