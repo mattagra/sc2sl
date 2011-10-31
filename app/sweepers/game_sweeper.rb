2c92cf4710caaf87daf30f813dea59c609ebb9fc
= <mrdakadoc@gmail.com>
2011-10-31 13:49:23 -0700

    adding maintenance page

--- app/sweepers/game_sweeper.rb
+++ app/sweepers/game_sweeper.rb
@@ -20,5 +20,6 @@ class GameSweeper < ActionController::Caching::Sweeper
     # Expire a fragment
     expire_fragment('recent_games')
     expire_fragment('season_rankings')
+    expire_fragment('season_rankings')
   end
 end
