b02b53b04bcaa6ca55d3dcf3d99965daf3ecb146
= <mrdakadoc@gmail.com>
2012-01-11 16:38:59 -0800

    working on popup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@ $('a[data-popup]').live('click', function(e) {
   var left = screen.width/2
   var top = screen.height/2
   
-  window.open($(this)[0].href, $(this).html(), $(this).attr("data-popup")+ ',top=' + top + ',left=' + left );
+  window.open($(this)[0].href, $(this).html(), $(this).attr("data-popup")+ ',top = ' + top + ',left = ' + left );
   //window.open($(this)[0].href );
   e.preventDefault();
 });
