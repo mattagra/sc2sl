29a22af79b703377f8f80dd44c9e2294d378add6
Michael Darmousseh <michael.darmousseh@gmail.com>
2012-01-29 09:45:34 -0800

    fix replay sorting

--- app/models/game.rb
+++ app/models/game.rb
@@ -21,7 +21,7 @@ class Game < ActiveRecord::Base
   #None necesary for games. We want to be able to schedule games.
 
   def self.paginated(page=1,offset=20)
-    order('id desc').limit(offset).offset((page - 1) * offset)
+    order('games.id asc').limit(offset).offset((page - 1) * offset)
   end
   
 
