a255701a5ea383b157cbff2d15e12a05cd153cb0
Michael Darmousseh <michael.darmousseh@gmail.com>
2012-03-28 16:10:10 -0700

    heroku config file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,8 +6,8 @@ Sc2sl::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
@@ -21,12 +21,12 @@ Sc2sl::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
   
-  config.logger = Logger.new(config.paths.log.first, 10, 5242880)
+  #config.logger = Logger.new(config.paths.log.first, 10, 5242880)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
@@ -40,7 +40,7 @@ Sc2sl::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
 
   # Enable threaded mode
