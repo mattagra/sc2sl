8ebec03b905ca403747895eff3957978601ea9b4
Michael Darmousseh <michael.darmousseh@gmail.com>
2012-03-28 14:49:50 -0700

    removed vendor

--- vendor/bundle/ruby/1.8/bin/sqlite3_ruby.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "E:/source/sc2sl/vendor/bundle/ruby/1.8/bin/sqlite3_ruby" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"ruby.exe" "%~dpn0" %*
